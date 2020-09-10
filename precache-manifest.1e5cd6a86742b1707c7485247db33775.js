114be9cc0f40b0795a8e9613605b299ae6f8fd28
vinipoo9 <yossi.povalaev@gmail.com>
2020-09-10 16:21:27 +0300

    Updates

---
+++ precache-manifest.1e5cd6a86742b1707c7485247db33775.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3fea3e8e2688cf92a9909516878f40f",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "0271bcc47a78e5346f96",
+    "url": "/react-deploy/static/css/main.b1b74cf1.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0271bcc47a78e5346f96",
+    "url": "/react-deploy/static/js/main.7d8dfd08.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "f53834be17f6367b6aeb33dd87c48dd1",
+    "url": "/react-deploy/static/media/pic-for-site.f53834be.png"
+  }
+]);
